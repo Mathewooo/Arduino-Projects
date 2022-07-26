c9e666a091711a918cf06de4b02cfd63bf43ce7c
Mathewooo <matej.sablica@gmail.com>
2022-07-26 19:04:53 +0200

    Initial Commit

---
+++ Dimmer Control.swift4a/main.swift
@@ -0,0 +1,22 @@
+import AVR
+
+typealias IntegerLiteralType = UInt8
+
+let analogPin: Pin = A0
+let ledPin: Pin = D11
+let readsPerSecond: Milliseconds = 25
+var currentAnalogValue: UInt8 = 0
+
+pinMode(pin: ledPin, mode: OUTPUT)
+SetupSerial()
+delay(milliseconds: 250)
+
+let validSetup: Bool = (readsPerSecond != 0)
+
+while(validSetup) {
+    analogReadAsync(pin: analogPin) { value in
+        currentAnalogValue = UInt8(value / 4)
+        analogWrite(pin: ledPin, value: currentAnalogValue)
+    }
+    delay(milliseconds: 1000 / readsPerSecond)
+}
---
+++ Fade.swift4a/main.swift
@@ -0,0 +1,31 @@
+import AVR
+
+typealias IntegerLiteralType = UInt8
+
+let ledPin: Pin = D11
+
+let fadesPerSecond: Milliseconds = 80
+let fadeOnStep: Int16 = 7
+let fadeOffStep: Int16 = 5
+let minBrightness: Int16 = 0
+let maxBrightness: Int16 = 255
+
+var currentBrightness: Int16 = minBrightness
+var currentBrightnessOffset: Int16 = fadeOnStep
+
+pinMode(pin: ledPin, mode: OUTPUT)
+
+let validSetup: Bool = (fadesPerSecond != 0)
+
+while(validSetup) {
+    analogWrite(pin: ledPin, value: UInt8(currentBrightness))
+    currentBrightness = currentBrightness + currentBrightnessOffset
+    if (currentBrightness <= minBrightness) {
+        currentBrightness = minBrightness
+        currentBrightnessOffset = fadeOnStep
+    } else if (currentBrightness >= maxBrightness) {
+        currentBrightness = maxBrightness
+        currentBrightnessOffset = -fadeOffStep
+    }
+    delay(milliseconds: 1000 / fadesPerSecond)
+}
